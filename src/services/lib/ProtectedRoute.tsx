6bb16fe3d81053c1bc208ad54fe179a361ef1d60
Julien MARTIN <ju.martin@outlook.com>
2025-01-20 09:54:09 +0100

    add protectedRoute

---
+++ src/services/lib/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+
+const ProtectedRoute = () => {
+    const location = useLocation();
+
+    // Récupérer le token (par exemple, depuis les cookies ou le stockage local)
+    const token = document.cookie.split('; ').find(row => row.startsWith('devis_token='))?.split('=')[1];
+
+    // Si pas de token, redirection vers /login
+    if (!token) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return <Outlet />;
+};
+
+export default ProtectedRoute;
--- src/services/lib/routes.tsx
+++ src/services/lib/routes.tsx
@@ -13,23 +13,27 @@ import PrestationsPage from "@/scenes/PrestationsPage";
 import ProduitsPage from "@/scenes/ProduitsPage";
 import DevisPage from "@/scenes/devis/DevisPage";
 import DevisDetailPage from "@/scenes/devis/DevisDetailPage";
+import ProtectedRoute from "@/services/lib/ProtectedRoute";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path={"/register"} element={<RegisterPage />} />
-            <Route path={"/adresses"} element={<AdressesPage />} />
-            <Route path={"/cgu"} element={<CguPage />} />
-            <Route path={"/clients"} element={<ClientsPage />} />
-            <Route path={"/contact"} element={<ContactPage />} />
-            <Route path={"/dashboard"} element={<DashboardPage />} />
-            <Route path={"/entreprises"} element={<EntreprisesPage />} />
-            <Route path={"/prestations"} element={<PrestationsPage />} />
-            <Route path={"/produits"} element={<ProduitsPage />} />
-            <Route path={"/devis"} element={<DevisPage />} />
-            <Route path={"/devis/:id"} element={<DevisDetailPage />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path={"/register"} element={<RegisterPage />} />
+                <Route path={"/cgu"} element={<CguPage />} />
+                <Route path={"/contact"} element={<ContactPage />} />
+
+                <Route element={<ProtectedRoute />}>
+                        <Route path={"/adresses"} element={<AdressesPage />} />
+                        <Route path={"/clients"} element={<ClientsPage />} />
+                        <Route path={"/dashboard"} element={<DashboardPage />} />
+                        <Route path={"/entreprises"} element={<EntreprisesPage />} />
+                        <Route path={"/prestations"} element={<PrestationsPage />} />
+                        <Route path={"/produits"} element={<ProduitsPage />} />
+                        <Route path={"/devis"} element={<DevisPage />} />
+                        <Route path={"/devis/:id"} element={<DevisDetailPage />} />
+                </Route>
         </Routes>
     );
 };
